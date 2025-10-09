8eef529637d59a5c7886a9b0779fa9c04b093967
Smurfhvr8ght <andres.winson@binus.ac.id>
2025-10-09 15:04:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function handleLogin(event) {
+    event.preventDefault();
+
+    // Simulate successful login (you can add validation here later)
+    // Redirect to HomePage.html
+    window.location.href = "HomePage.html";
+}
